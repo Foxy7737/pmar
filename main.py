5e468a50b7c38cc045d8222cf42e594bad5cfff0
Foxy7737 <107135073+Foxy7737@users.noreply.github.com>
2022-06-08 18:23:24 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("A C5 A C5 A C5 A C5 ", 120)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+})
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-input.onButtonPressed(Button.A, function () {
-    music.playMelody("A C5 A C5 A C5 A C5 ", 120)
-})
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    music.playMelody("C D G E A B C G ", 281)
+    for (let index = 0; index < 1; index++) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+    for (let index = 0; index < 1; index++) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+    basic.pause(2000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
 })
