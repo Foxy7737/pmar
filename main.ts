98648deee33c62b3e40a6207d799f9d29bc0b764
Foxy7737 <107135073+Foxy7737@users.noreply.github.com>
2022-06-08 18:12:38 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("A C5 A C5 A C5 A C5 ", 120)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
